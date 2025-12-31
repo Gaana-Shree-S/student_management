e88c01e38b233d7bd0756ae171c71f7f70ddecd8
Gaana Shree S <146619352+Gaana-Shree-S@users.noreply.github.com>
2025-12-31 11:57:45 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import LandingPage from "./LandingPage";
 import "./index.css";
+
+const Root = () => {
+  const [started, setStarted] = useState(false);
+
+  return started ? <App /> : <LandingPage onGetStarted={() => setStarted(true)} />;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Root />);
